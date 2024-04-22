a421b2eb65d026ddf863f4daf1f22f390d1fbe2b
mustaeen <136145181+must108@users.noreply.github.com>
2024-04-22 03:41:21 -0400

    almost done with mp3

--- miniproject3.R
+++ miniproject3.R
@@ -3,8 +3,34 @@ library(readxl)
 bank <- read_excel("./bank.xlsx")
 
 dataset <- data.frame(bank)
-print(dataset)
+# print(dataset) # nolint
 attach(dataset)
 
 # Question 1:
-plot(Income, Rating)
+plot(Income, Rating, xlab = "Income ($10,000s)",
+     ylab = "Credit Score (in points)",
+     main = "Income v.s. Credit Score Scatterplot")
+# this answers part b
+
+bank_model <- lm(Rating ~ Income, data = dataset)
+print(summary(bank_model))
+print(anova(bank_model))
+# parts c, e, j
+
+print(confint(bank_model, level = 0.95))
+# for part h
+
+print(cor(Income, Rating))
+# for part i
+
+plot(bank_model)
+print(shapiro.test(bank_model$residuals))
+# for part k
+
+new_val <- data.frame(Income = 56000)
+pred_val <- predict(bank_model, newdata = new_val)
+print(pred_val)
+
+multi_model <- lm(Rating ~ Income + Age + Education, data = dataset)
+print(summary(multi_model))
+print(anova(multi_model))
